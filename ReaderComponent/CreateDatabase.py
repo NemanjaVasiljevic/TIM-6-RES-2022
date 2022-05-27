c403add82d059dc8b1e76e2421e7cdf3805a0301
NemanjaVasiljevic <nemus.vasiljevic@gmail.com>
2022-05-27 13:31:32 +0200

    reader_componente 1.0

---
+++ ReaderComponent/CreateDatabase.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "127.0.0.1", #stavi localhost ili svoj ip vrv ce biti isti kao i ovaj proveri u MySql Workbench
+    user = "root",
+    passwd = "1969" #stavi svoju sifru
+)
+
+myCursor = db.cursor()
+
+def CreateDatabase():
+    myCursor.execute("CREATE DATABASE readerDB")
---
+++ ReaderComponent/server.py
@@ -0,0 +1,14 @@
+from CreateDatabase import CreateDatabase
+from CreateTables import CreateTables
+from DatabaseFunctions import (AddToTable)
+
+
+
+##### ovo se radi samo prvi put prilikom pokretanja posle samo zakomenterises
+#CreateDatabase()
+#CreateTables()
+#############################################################################
+
+## problem kod importovanje Database foldera nece da ga vidi kao modul pa sam kopirao samo u ReaderComponent sve radi testiranja ispravnosti
+
+AddToTable("value#0001","CODE_ANALOG")
