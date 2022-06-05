b43ed2d8d891eb4694e1b402c78b39d13648341e
Comi15 <milovan.mica@hotmail.com>
2022-06-06 01:54:51 +0200

    Dodata klasa CollectionDescription

--- Model/DataModel.py
+++ Model/DataModel.py
@@ -9,6 +9,8 @@ class Data:
     def __init__(self, value, code):
         self.value = value
         self.code = code
+    def __str__(self):
+        return f"Code: {self.code} Value: {self.value}"    
 
 class Reader:
     def __init__(self,port,database):
@@ -29,9 +31,25 @@ class Reader:
         msg = clientsocket.recv(4098)
         data = pickle.loads(msg)
         try:
-            AddToTable(data.value, data.code, database)
             print("Recieved from ReplicatorReciver:")
-            print(f"Code : {data.code}   Value: {data.value}")
+            print(f"Code : {data.historicalCollection[-1].code}   DataSet: {data.dataSet}")
+            print("HistoricalCollection:")
+            print(*data.historicalCollection, sep="\n")
+            AddToTable(data.historicalCollection[-1].value, data.historicalCollection[-1].code, "dataset1")
+            print("Succsessfully added object to the table")
 
         except:
             return F"Whoops. Something went wrong with writting in base!"
+	
+class CollectionDescription:
+        def __init__(self,historicalCollection,code):
+            if code == "CODE_ANALOG" or "CODE_DIGITAL":
+                self.dataSet = 1
+            elif code ==  "CODE_CUSTOM" or "CODE_LIMITSET":
+                self.dataSet = 2
+            elif code ==  "CODE_SINGLENOE" or "CODE_MULTIPLENODE":
+                self.dataSet = 3
+            elif code ==  "CODE_CONSUMER" or "CODE_SOURCE":
+                self.dataSet = 4    
+
+            self.historicalCollection = historicalCollection	
