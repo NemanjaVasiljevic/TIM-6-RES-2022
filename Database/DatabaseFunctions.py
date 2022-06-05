4be6bb5732a34be3e21cd786733f24d31b082af7
Comi15 <milovan.mica@hotmail.com>
2022-06-06 01:54:13 +0200

    Promenjeni parametri za moju bazu

--- Database/DatabaseFunctions.py
+++ Database/DatabaseFunctions.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host = "127.0.0.1",
+    host = "localhost",
     user = "root",
-    passwd = "1969",
+    passwd = "12345",
     database = "readerDB"
 )
 
