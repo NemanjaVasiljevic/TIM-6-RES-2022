c97ec857c48b976af7716bd2b245ce5303e28526
Comi15 <milovan.mica@hotmail.com>
2022-06-06 11:45:40 +0200

    Citanje iz baze implementirano

--- ReaderComponent/Reader.py
+++ ReaderComponent/Reader.py
@@ -17,3 +17,11 @@ while True:
     
    
 clientsocket.close()
+
+''' Citanje iz baze poslednjih  vrednosti izabranih kodova
+
+dataRead1, dataRead2 = r1.ReadData("CODE_DIGITAL", "CODE_ANALOG")
+dataRead1 = Data(dataRead1[0],dataRead1[1])
+dataRead2 = Data(dataRead2[0],dataRead2[1])
+print(F"Value 1 : {dataRead1.str()}\n Value 2: {dataRead2.str()}")
+ '''
