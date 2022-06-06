e77180e9d0cfc002818f6944ce669dbde5320e51
Comi15 <milovan.mica@hotmail.com>
2022-06-06 11:42:51 +0200

    Implementirano citanje iz baze poslednjih vrednosti code-ova

--- Database/DatabaseFunctions.py
+++ Database/DatabaseFunctions.py
@@ -14,9 +14,22 @@ def AddToTable(value, code, database):
     myCursor.execute(f"INSERT INTO {database} (value, code) VALUES (%s, %s)", (value,code))
     db.commit()
 
-def SeeTable():
-    myCursor.execute("SELECT * FROM dataset1")
+def ReadFromTable(code1, code2, database):
+
+    myCursor.execute(F"SELECT * FROM {database} WHERE code in ('{code1}', '{code2}') order by id desc")
+    first = 1
+
     for x in myCursor:
-        print(x)
+
+        if(first == 1):
+            data1 = x
+            first = 0
+            
+        if(x[1] != data1[1]):
+            data2 = x
+            break
+
+    
+    return data1,data2
     
 
