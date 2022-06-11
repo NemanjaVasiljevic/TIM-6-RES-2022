24666a12a925d87ff263fae52518c10f232b8c57
Gvoz29 <branislavgvozdenov29@gmail.com>
2022-06-11 15:43:13 +0200

    Dodavanje kostura klijentske aplikacije

---
+++ WriterComponent/Client.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('../')
+import socket,pickle,time,random
+import os
+
+
+gasi=False
+
+while(True):
+    print("Izaberite neku od sledecih opcija:")
+    print("1. Paljenje novog Writer-a")
+    print("2. Gasenje Writer-a")
+    print("3. Citanje iz baze po istoriji")
+    print("4. Citanje iz baze po odredjenim kodovima")
+    q1=input()
+    if(q1=="1"):  
+      os.system("start cmd /k python Writer.py")
+    elif(q1=="2"):
+      gasi==True
+    elif(q1=="3"):
+      dateFrom=input("Unesite datum od kada zelite da citate iz baze: ")
+      dateTo=input("Unesite datum do kog zelite da citate iz baze: ")
+    elif(q1=="4"):  
+      print("Izaberite dva koda koje zelite da procitate iz baze:")
+      print("1. CODE_ANALOG")
+      print("2. CODE_DIGITAL")
+      print("3. CODE_CUSTOM")
+      print("4. CODE_LIMITSET")
+      print("5. CODE_SINGLENOE")
+      print("6. CODE_MULTIPLENODE")
+      print("7. CODE_CONSUMER")
+      print("8. CODE_SOURCE")
+      kod1=input()
+      kod2=input()
+       
--- WriterComponent/Writer.py
+++ WriterComponent/Writer.py
@@ -1,8 +1,12 @@
 import sys
 sys.path.append('../')
-import socket,pickle,time,random
+import socket,pickle,time,random,os
 from Model.DataModel import Data
 listNames = ["CODE_ANALOG","CODE_DIGITAL","CODE_CUSTOM","CODE_LIMITSET","CODE_SINGLENOE","CODE_MULTIPLENODE","CODE_CONSUMER","CODE_SOURCE"]
+from WriterComponent.Client import gasi
+
+if(gasi==True):
+    os.close
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((socket.gethostname(),8000))
@@ -13,3 +17,4 @@ while True:
     s.send(data_string)
     print("Sending to ReplicatorSender:")
     print(f"Code : {variable.code}   Value: {variable.value}")
+
