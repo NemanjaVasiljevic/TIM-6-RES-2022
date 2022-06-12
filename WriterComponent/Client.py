2ce15d5f251148854a92eeef37ad6a969152a9d4
Gvoz29 <branislavgvozdenov29@gmail.com>
2022-06-12 17:42:48 +0200

    Dorada klijenta i Writer-a

--- WriterComponent/Client.py
+++ WriterComponent/Client.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append('../')
-import socket,pickle,time,random
-import os
-
+import socket,pickle,os
+from Model.DataModel import Data, HistoricalValue,Request
 
 gasi=False
-
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((socket.gethostname(),2000))
 while(True):
     print("Izaberite neku od sledecih opcija:")
     print("1. Paljenje novog Writer-a")
@@ -16,20 +16,23 @@ while(True):
     if(q1=="1"):  
       os.system("start cmd /k python Writer.py")
     elif(q1=="2"):
-      gasi==True
+      request = Request("CloseWriter",data)
+      data_string = pickle.dumps(request)
+      s.send(data_string)
     elif(q1=="3"):
       dateFrom=input("Unesite datum od kada zelite da citate iz baze: ")
       dateTo=input("Unesite datum do kog zelite da citate iz baze: ")
+      print("Unesite neki od sledecih kodova: CODE_ANALOG,CODE_DIGITAL,CODE_CUSTOM,CODE_LIMITSET,CODE_SINGLENOE,CODE_MULTIPLENODE,CODE_CONSUMER,CODE_SOURCE")
+      code=input()
+      data = HistoricalValue(code,dateFrom,dateTo )
+      request = Request("ReadHistorical",data)
+      data_string = pickle.dumps(request)
+      s.send(data_string)
     elif(q1=="4"):  
-      print("Izaberite dva koda koje zelite da procitate iz baze:")
-      print("1. CODE_ANALOG")
-      print("2. CODE_DIGITAL")
-      print("3. CODE_CUSTOM")
-      print("4. CODE_LIMITSET")
-      print("5. CODE_SINGLENOE")
-      print("6. CODE_MULTIPLENODE")
-      print("7. CODE_CONSUMER")
-      print("8. CODE_SOURCE")
+      print("Izaberite dva koda koje zelite da procitate iz baze, kodovi su:CODE_ANALOG,CODE_DIGITAL,CODE_CUSTOM,CODE_LIMITSET,CODE_SINGLENOE,CODE_MULTIPLENODE,CODE_CONSUMER,CODE_SOURCE")
       kod1=input()
       kod2=input()
-       
+    else:   
+      request = Request("WriteRequest",data)
+      data_string = pickle.dumps(request)
+      s.send(data_string)
--- WriterComponent/Writer.py
+++ WriterComponent/Writer.py
@@ -1,20 +1,56 @@
 import sys
 sys.path.append('../')
 import socket,pickle,time,random,os
-from Model.DataModel import Data
+from Model.DataModel import Data, HistoricalValue,Request
 listNames = ["CODE_ANALOG","CODE_DIGITAL","CODE_CUSTOM","CODE_LIMITSET","CODE_SINGLENOE","CODE_MULTIPLENODE","CODE_CONSUMER","CODE_SOURCE"]
-from WriterComponent.Client import gasi
 
-if(gasi==True):
-    os.close
-
+#konktovanje na replicatorSender
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),8000))
-while True:
-    time.sleep(2)
-    variable = Data(random.randint(1,500),random.choice(listNames))
-    data_string = pickle.dumps(variable)
-    s.send(data_string)
-    print("Sending to ReplicatorSender:")
-    print(f"Code : {variable.code}   Value: {variable.value}")
+s.connect((socket.gethostname(),7000))
 
+#Soket za komunikaciju sa klijentom
+sClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+sClient.bind((socket.gethostname(),2000))
+sClient.listen(1)
+SocketClient, addressClient = s.accept()
+option = 1
+
+while True:
+    
+    msgClient = SocketClient.recv(4098)
+    recivedClient = pickle.loads(msgClient)
+
+
+    if recivedClient.request == "ReadHistorical":
+        print("Dobio read request")
+        
+        s.send(data_string)
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((socket.gethostname(),1234))
+        s.listen(1)
+        print("Waiting for connection...")
+
+        readerSocket, address = s.accept()
+        print(f"Connection established from address {address}")
+
+        msg = readerSocket.recv(4098)
+        recived = pickle.loads(msg)
+
+        for x in recived:
+            tempData = Data(x[0],x[1])
+            print(tempData)
+
+        option = 1
+        time.sleep(10)
+
+    elif recivedClient.request=="WriteRequest":
+        time.sleep(2)
+        data = Data(random.randint(1,500),random.choice(listNames))
+        request = Request("WriteRequest",data)
+        data_string = pickle.dumps(request)
+        s.send(data_string)
+        print("Sending to ReplicatorSender:")
+        print(f"Code : {request.data.code}   Value: {request.data.value}")    
+    elif recivedClient.request=="CloseWriter":
+        os.close()
